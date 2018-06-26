6cb200ee86c1268503d029aa246d1c9bf0703904
IrisAndMimi <my1ris227@outlook.com>
2018-06-26 15:36:09 +0800

    commit

--- src/com/exam/menusystem.java
+++ src/com/exam/menusystem.java
@@ -19,17 +19,20 @@ public class menusystem {
 	}
 
 	public void on() {
-		int idlsr = -1;
-		System.out.print("請輸入餐點:");
-		idlsr = scanner.nextInt();
-		int atlsr = -1;
-		System.out.print("請輸入數量:");
-		atlsr = scanner.nextInt();
-//		foods.contains(idlsr);
-		System.out.println("目前餐點:");
-		food = foods.get(idlsr-1);
-		System.out.println(food.getName()+" "+atlsr+"份");
-	
+		int idlsr = -2;
+		while(idlsr != -1){
+				idlsr = -1;
+				System.out.print("請輸入餐點:");
+				idlsr = scanner.nextInt();
+				int atlsr = -1;
+				System.out.print("請輸入數量:");
+				atlsr = scanner.nextInt();
+		//		foods.contains(idlsr);
+				System.out.println("目前餐點:");
+				food = foods.get(idlsr-1);
+				System.out.println(food.getName()+" "+atlsr+"份");
+		}		
+		
 	}
 
 	public void writer() {
